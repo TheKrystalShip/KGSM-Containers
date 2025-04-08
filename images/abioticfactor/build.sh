95481f4ed376515b04fdfb54362549433c3a85bb
Cristian Moraru <cristian.moraru@live.com>
2025-04-08 19:23:59 +0200

    Added abioticfactor image

---
+++ images/abioticfactor/build.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+local_image="abioticfactor"
+remote_image="ghcr.io/thekrystalship/${local_image}"
+
+echo "Building ${local_image}..."
+docker build -t "${local_image}" .
+
+echo "Tagging ${local_image} image..."
+docker image tag "${local_image}" "${remote_image}"
+
+if [[ "$1" == "--push" ]]; then
+  echo "Pushing ${remote_image}..."
+  docker push "${remote_image}"
+fi
