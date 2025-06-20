7652693fbf1ab2626a70b8a69e2827d9ee363831
Cristian Moraru <cristian.moraru@live.com>
2025-06-20 12:23:13 +0200

    Updated build script to include pushing to DockerHub

--- images/abioticfactor/build.sh
+++ images/abioticfactor/build.sh
@@ -1,15 +1,24 @@
 #!/usr/bin/env bash
 
 local_image="abioticfactor"
-remote_image="ghcr.io/thekrystalship/${local_image}"
+# GitHub Container Registry path
+ghcr_image="ghcr.io/thekrystalship/${local_image}"
+# Docker Hub path
+dockerhub_image="thekrystalship/${local_image}"
 
 echo "Building ${local_image}..."
 docker build -t "${local_image}" .
 
-echo "Tagging ${local_image} image..."
-docker image tag "${local_image}" "${remote_image}"
+echo "Tagging ${local_image} for GitHub Container Registry..."
+docker image tag "${local_image}" "${ghcr_image}"
+
+echo "Tagging ${local_image} for Docker Hub..."
+docker image tag "${local_image}" "${dockerhub_image}"
 
 if [[ "$1" == "--push" ]]; then
-  echo "Pushing ${remote_image}..."
-  docker push "${remote_image}"
+  echo "Pushing ${ghcr_image} to GitHub Container Registry..."
+  docker push "${ghcr_image}"
+
+  echo "Pushing ${dockerhub_image} to Docker Hub..."
+  docker push "${dockerhub_image}"
 fi
